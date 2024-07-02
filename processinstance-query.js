748f9b46361ee44a97b8fa7475f7261957a97816
Martin Möllenbeck <martin.moellenbeck@5minds.de>
2024-07-02 23:18:29 +0200

    Add nodes to query instances and defintions

---
+++ processinstance-query.js
@@ -0,0 +1,48 @@
+const process = require('process');
+const EventEmitter = require('node:events');
+
+const engine_client = require('@5minds/processcube_engine_client');
+
+module.exports = function(RED) {
+    function ProcessinstanceQuery(config) {
+        RED.nodes.createNode(this,config);
+        var node = this;
+        var flowContext = node.context().flow;
+        var nodeContext = node.context();
+
+        this.engine = this.server = RED.nodes.getNode(config.engine);
+
+        const engineUrl = this.engine?.url || process.env.ENGINE_URL || 'http://engine:8000';
+
+        var client = nodeContext.get('client');
+
+        if (!client) {
+            nodeContext.set('client', new engine_client.EngineClient(engineUrl));
+            client = nodeContext.get('client');
+        }
+
+        var eventEmitter = flowContext.get('emitter');
+
+        if (!eventEmitter) {
+            flowContext.set('emitter', new EventEmitter());
+            eventEmitter = flowContext.get('emitter');
+        }
+
+        node.on("close", async () => {
+            client.dispose();
+            client = null;
+        });
+
+        node.on('input', function(msg) {
+            const query = RED.util.evaluateNodeProperty(config.query, config.query_type, node, msg)
+
+            client.processInstances.query(query).then((matchingInstances) => {
+
+                msg.payload = matchingInstances;
+
+                node.send(msg);
+            });
+        });
+    }
+    RED.nodes.registerType("processinstance-query", ProcessinstanceQuery);
+}
