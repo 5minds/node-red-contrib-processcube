197b34d76e6bcda9ba0f294da3c61db13c5bfef2
luisthieme <luis.thieme@5minds.de>
2024-07-24 15:44:09 +0200

    use client from config nodes in all other nodes

--- processdefinition-query.js
+++ processdefinition-query.js
@@ -1,31 +1,23 @@
-const process = require('process');
-const EventEmitter = require('node:events');
+const process = require("process");
+const EventEmitter = require("node:events");
 
-const engine_client = require('@5minds/processcube_engine_client');
+const engine_client = require("@5minds/processcube_engine_client");
 
-module.exports = function(RED) {
+module.exports = function (RED) {
     function ProcessdefinitionQuery(config) {
-        RED.nodes.createNode(this,config);
+        RED.nodes.createNode(this, config);
         var node = this;
         var flowContext = node.context().flow;
-        var nodeContext = node.context();
 
         this.engine = this.server = RED.nodes.getNode(config.engine);
 
-        const engineUrl = this.engine?.url || process.env.ENGINE_URL || 'http://engine:8000';
+        const client = this.engine.getEngineClient();
 
-        var client = nodeContext.get('client');
-
-        if (!client) {
-            nodeContext.set('client', new engine_client.EngineClient(engineUrl));
-            client = nodeContext.get('client');
-        }
-
-        var eventEmitter = flowContext.get('emitter');
+        var eventEmitter = flowContext.get("emitter");
 
         if (!eventEmitter) {
-            flowContext.set('emitter', new EventEmitter());
-            eventEmitter = flowContext.get('emitter');
+            flowContext.set("emitter", new EventEmitter());
+            eventEmitter = flowContext.get("emitter");
         }
 
         node.on("close", async () => {
@@ -33,38 +25,48 @@ module.exports = function(RED) {
             client = null;
         });
 
-        node.on('input', function(msg) {
-            let query = RED.util.evaluateNodeProperty(config.query, config.query_type, node, msg)
+        node.on("input", function (msg) {
+            let query = RED.util.evaluateNodeProperty(
+                config.query,
+                config.query_type,
+                node,
+                msg
+            );
             query = {
                 ...query,
-                identity: node.server.identity
+                identity: node.server.identity,
             };
-            
-            client.processDefinitions.getAll(query).then((matchingProcessDefinitions) => {
 
-    
-                if (config.models_only && matchingProcessDefinitions.totalCount > 0) {
-                    let models = [];
+            client.processDefinitions
+                .getAll(query)
+                .then((matchingProcessDefinitions) => {
+                    if (
+                        config.models_only &&
+                        matchingProcessDefinitions.totalCount > 0
+                    ) {
+                        let models = [];
 
-                    matchingProcessDefinitions.processDefinitions.forEach(processDefinition => {
-                        processDefinition.processModels.forEach(model => {
-                            models.push(model);
-                        });
-                    });
+                        matchingProcessDefinitions.processDefinitions.forEach(
+                            (processDefinition) => {
+                                processDefinition.processModels.forEach(
+                                    (model) => {
+                                        models.push(model);
+                                    }
+                                );
+                            }
+                        );
 
-                    msg.payload = {
-                        models: models,
-                        totalCount: models.length
-                    };
+                        msg.payload = {
+                            models: models,
+                            totalCount: models.length,
+                        };
+                    } else {
+                        msg.payload = matchingProcessDefinitions;
+                    }
 
-                } else {
-                    msg.payload = matchingProcessDefinitions;
-                }
-
-
-                node.send(msg);
-            });
+                    node.send(msg);
+                });
         });
     }
     RED.nodes.registerType("processdefinition-query", ProcessdefinitionQuery);
-}
+};
