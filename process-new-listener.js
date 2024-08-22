7fe78b5c65d405cba678a4dbaca1dfd110d046bd
luisthieme <luis.thieme@5minds.de>
2024-08-22 08:28:18 +0200

    register node

---
+++ process-new-listener.js
@@ -0,0 +1,71 @@
+module.exports = function (RED) {
+    function ProcessNewListener(config) {
+        RED.nodes.createNode(this, config);
+        var node = this;
+        node.engine = RED.nodes.getNode(config.engine);
+
+        const register = async () => {
+            const client = node.engine.engineClient;
+
+            if (!client) {
+                node.error('No engine configured.');
+                return;
+            }
+
+            let currentIdentity = node.engine.identity;
+
+            let subscription;
+
+            if (node.engine.isIdentityReady()) {
+                subscription = await client.notifications.onProcessStarted(
+                    (processNotification) => {
+                        // node.send({
+                        //     payload: {
+                        //         flowNodeInstanceId: userTaskWaitingNotification.flowNodeInstanceId,
+                        //         userTaskEvent: userTaskWaitingNotification,
+                        //         action: 'new',
+                        //         type: 'usertask',
+                        //     },
+                        // });
+                        console.log(processNotification);
+                    },
+                    { identity: currentIdentity }
+                );
+            }
+
+            node.engine.registerOnIdentityChanged(async (identity) => {
+                if (subscription) {
+                    client.notifications.removeSubscription(subscription, currentIdentity);
+                }
+
+                currentIdentity = identity;
+
+                subscription = await client.notifications.onProcessStarted(
+                    (processNotification) => {
+                        // node.send({
+                        //     payload: {
+                        //         flowNodeInstanceId: userTaskWaitingNotification.flowNodeInstanceId,
+                        //         userTaskEvent: userTaskWaitingNotification,
+                        //         action: 'new',
+                        //         type: 'usertask',
+                        //     },
+                        // });
+                        console.log(processNotification);
+                    },
+                    { identity: currentIdentity }
+                );
+            });
+
+            node.on('close', () => {
+                if (node.engine && node.engine.engineClient && client) {
+                    client.notifications.removeSubscription(subscription, currentIdentity);
+                }
+            });
+        };
+
+        if (node.engine) {
+            register();
+        }
+    }
+    RED.nodes.registerType('process-new-listener', ProcessNewListener);
+};
