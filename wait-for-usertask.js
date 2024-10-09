92414db2af7038a109fad05686005c8b7eef087c
Martin Möllenbeck <martin.moellenbeck@5minds.de>
2024-10-09 15:40:23 +0200

    Add wait-for-usertask ;-)

--- wait-for-usertask.js
+++ wait-for-usertask.js
@@ -1,91 +1,89 @@
-const process = require('process');
-
-const engine_client = require('@5minds/processcube_engine_client');
-const { userInfo } = require('os');
-
 module.exports = function (RED) {
     function WaitForUsertask(config) {
         RED.nodes.createNode(this, config);
         var node = this;
-        var nodeContext = node.context();
-        
-        this.engine = this.server = RED.nodes.getNode(config.engine);
 
-        const engineUrl = this.engine?.url || process.env.ENGINE_URL || 'http://engine:8000';
+        const engine = RED.nodes.getNode(config.engine);
 
-        var client = nodeContext.get('client');
+        const client = engine.engineClient;
 
-        if (!client) {
-            nodeContext.set('client', new engine_client.EngineClient(engineUrl));
-            client = nodeContext.get('client');
-        }
-
-
-        let currentIdentity = null;
         let subscription = null;
+        let currentIdentity = engine.identity;
+        let subscribe = null;
 
         node.on('input', async function (msg) {
-            if (node.server) {
-                currentIdentity = node.server.identity;
-            } else {
-                node.error("No processcube_config node found.")
-                return;
-            }
-
-            const query = RED.util.evaluateNodeProperty(config.query, config.query_type, node, msg);
-
-            subscription = await client.userTasks.onUserTaskWaiting(async (userTaskWaitingNotification) => {
-
-                const newQuery = {
-                    'flowNodeInstanceId': userTaskWaitingNotification.flowNodeInstanceId,
-                    ...query
-                };
-
-                const matchingFlowNodes = await client.userTasks.query(newQuery, { identity: currentIdentity });
-
-                if (matchingFlowNodes.userTasks && matchingFlowNodes.userTasks.length == 1) {
-                    const userTask = matchingFlowNodes.userTasks[0];
-
-                    msg.payload = { userTask: userTask };
-                    node.send(msg);
-                } else {
-                    // nothing todo - wait for next notification
+            subscribe = async () => {
+                if (!client) {
+                    node.error('No engine configured.');
+                    return;
                 }
 
-                // remove subscription
-                client.userTasks.removeSubscription(subscription, currentIdentity);
-            }, { identity: currentIdentity });
+                const query = RED.util.evaluateNodeProperty(config.query, config.query_type, node, msg);
 
-            console.debug('Handling old userTasks config.only_for_new', config.only_for_new);
+                subscription = await client.userTasks.onUserTaskWaiting(async (userTaskWaitingNotification) => {
 
-            if (config.only_for_new === false) {
-                // only check suspended user tasks
-                const suspendedQuery = {
-                    'state': 'suspended',
-                    ...query
-                };
+                    const newQuery = {
+                        'flowNodeInstanceId': userTaskWaitingNotification.flowNodeInstanceId,
+                        ...query
+                    };
 
-                const matchingFlowNodes = await client.userTasks.query(suspendedQuery, { identity: currentIdentity });
+                    const matchingFlowNodes = await client.userTasks.query(newQuery, { identity: currentIdentity });
 
-                if (matchingFlowNodes.userTasks && matchingFlowNodes.userTasks.length >= 1) {
-                    const userTask = matchingFlowNodes.userTasks[0];
+                    if (matchingFlowNodes.userTasks && matchingFlowNodes.userTasks.length == 1) {
+                        const userTask = matchingFlowNodes.userTasks[0];
 
-                    msg.payload = { userTask: userTask };
-                    node.send(msg);
+                        msg.payload = { userTask: userTask };
+                        node.send(msg);
+                    } else {
+                        // nothing todo - wait for next notification
+                    }
 
                     // remove subscription
                     client.userTasks.removeSubscription(subscription, currentIdentity);
-                } else {
-                    // let the *currentIdentity* be active
+                }, { identity: currentIdentity });
+
+                node.log({"Handling old userTasks config.only_for_new": config.only_for_new});
+
+                if (config.only_for_new === false) {
+                    // only check suspended user tasks
+                    const suspendedQuery = {
+                        'state': 'suspended',
+                        ...query
+                    };
+
+                    const matchingFlowNodes = await client.userTasks.query(suspendedQuery, { identity: currentIdentity });
+
+                    if (matchingFlowNodes.userTasks && matchingFlowNodes.userTasks.length >= 1) {
+                        const userTask = matchingFlowNodes.userTasks[0];
+
+                        msg.payload = { userTask: userTask };
+                        node.send(msg);
+
+                        // remove subscription
+                        client.userTasks.removeSubscription(subscription, currentIdentity);
+                    } else {
+                        // let the *currentIdentity* be active
+                    }
                 }
+            };
+
+            subscribe();
+        });
+
+        node.engine.registerOnIdentityChanged(async (identity) => {
+
+            if (subscription) {
+                client.userTasks.removeSubscription(subscription, currentIdentity);
+                currentIdentity = identity;
+                subscribe();
+            } else {
+                currentIdentity = identity;
             }
         });
 
         node.on("close", async () => {
-            if (client != null) {
+            if (client != null && subscription != null) {
                 client.userTasks.removeSubscription(subscription, currentIdentity);                
-                client.dispose();
-                client = null;
             }
         });
     }
