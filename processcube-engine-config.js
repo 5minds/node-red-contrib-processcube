4b06ce6723c66235e0caa0251427eb6af4fa54dc
luisthieme <luis.thieme@5minds.de>
2024-11-06 17:48:05 +0100

    remove logs

--- processcube-engine-config.js
+++ processcube-engine-config.js
@@ -2,8 +2,6 @@ const engine_client = require('@5minds/processcube_engine_client');
 const jwt = require('jwt-decode');
 const oidc = require('openid-client');
 
-const DELAY_FACTOR = 0.85;
-
 module.exports = function (RED) {
     function ProcessCubeEngineNode(n) {
         RED.nodes.createNode(this, n);
@@ -36,7 +34,7 @@ module.exports = function (RED) {
             }
         };
 
-        async function getFreshIdentity(url) {
+        async function getFreshIdentity(url, node) {
             try {
                 if (
                     !RED.util.evaluateNodeProperty(n.clientId, n.clientIdType, node) ||
@@ -44,9 +42,6 @@ module.exports = function (RED) {
                 ) {
                     return null;
                 }
-
-                node.log(RED.util.evaluateNodeProperty(n.clientId, n.clientIdType, node));
-                node.log(RED.util.evaluateNodeProperty(n.clientSecret, n.clientSecretType, node));
 
                 const res = await fetch(url + '/atlas_engine/api/v1/authority', {
                     method: 'GET',
@@ -56,23 +51,19 @@ module.exports = function (RED) {
                     },
                 });
 
-                const issuer = await oidc.Issuer.discover(await res.json());
+                const body = await res.json();
 
-                node.log('after issuer');
+                const issuer = await oidc.Issuer.discover(body);
 
                 const client = new issuer.Client({
                     client_id: RED.util.evaluateNodeProperty(n.clientId, n.clientIdType, node),
                     client_secret: RED.util.evaluateNodeProperty(n.clientSecret, n.clientSecretType, node),
                 });
 
-                node.log('after client');
-
                 const tokenSet = await client.grant({
                     grant_type: 'client_credentials',
                     scope: 'engine_etw engine_read engine_write',
                 });
-
-                node.log('after grant');
 
                 const accessToken = tokenSet.access_token;
                 const decodedToken = jwt.jwtDecode(accessToken);
@@ -82,9 +73,7 @@ module.exports = function (RED) {
                     userId: decodedToken.sub,
                 };
 
-                console.log('luis777', freshIdentity);
-
-                configNode.setIdentity(freshIdentity);
+                node.setIdentity(freshIdentity);
 
                 return freshIdentity;
             } catch (e) {
@@ -102,25 +91,7 @@ module.exports = function (RED) {
         });
 
         if (this.credentials.clientId && this.credentials.clientSecret) {
-            this.engineClient = new engine_client.EngineClient(this.url, () => getFreshIdentity(this.url));
-
-            // this.engineClient.applicationInfo
-            //     .getAuthorityAddress()
-            //     .then((authorityUrl) => {
-            //         startRefreshingIdentityCycle(
-            //             this.credentials.clientId,
-            //             this.credentials.clientSecret,
-            //             authorityUrl,
-            //             node
-            //         ).catch((reason) => {
-            //             console.error(reason);
-            //             node.error(reason);
-            //         });
-            //     })
-            //     .catch((reason) => {
-            //         console.error(reason);
-            //         node.error(reason);
-            //     });
+            this.engineClient = new engine_client.EngineClient(this.url, () => getFreshIdentity(this.url, node));
         } else {
             this.engineClient = new engine_client.EngineClient(this.url);
         }
@@ -132,85 +103,3 @@ module.exports = function (RED) {
         },
     });
 };
-
-// async function getFreshTokenSet(clientId, clientSecret, authorityUrl) {
-//     const issuer = await oidc.Issuer.discover(authorityUrl);
-
-//     const client = new issuer.Client({
-//         client_id: clientId,
-//         client_secret: clientSecret,
-//     });
-
-//     const tokenSet = await client.grant({
-//         grant_type: 'client_credentials',
-//         scope: 'engine_etw engine_read engine_write',
-//     });
-
-//     return tokenSet;
-// }
-
-// function getIdentityForExternalTaskWorkers(tokenSet) {
-//     const accessToken = tokenSet.access_token;
-//     const decodedToken = jwt.jwtDecode(accessToken);
-
-//     return {
-//         token: tokenSet.access_token,
-//         userId: decodedToken.sub,
-//     };
-// }
-
-// async function getExpiresInForExternalTaskWorkers(tokenSet) {
-//     let expiresIn = tokenSet.expires_in;
-
-//     if (!expiresIn && tokenSet.expires_at) {
-//         expiresIn = Math.floor(tokenSet.expires_at - Date.now() / 1000);
-//     }
-
-//     if (expiresIn === undefined) {
-//         throw new Error('Could not determine the time until the access token for external task workers expires');
-//     }
-
-//     return expiresIn;
-// }
-
-// /**
-//  * Start refreshing the identity in regular intervals.
-//  * @param {TokenSet | null} tokenSet The token set to refresh the identity for
-//  * @returns {Promise<void>} A promise that resolves when the timer for refreshing the identity is initialized
-//  * */
-// async function startRefreshingIdentityCycle(clientId, clientSecret, authorityUrl, configNode) {
-//     let retries = 5;
-
-//     const refresh = async () => {
-//         try {
-//             const newTokenSet = await getFreshTokenSet(clientId, clientSecret, authorityUrl);
-//             const expiresIn = await getExpiresInForExternalTaskWorkers(newTokenSet);
-//             const delay = expiresIn * DELAY_FACTOR * 1000;
-
-//             freshIdentity = getIdentityForExternalTaskWorkers(newTokenSet);
-
-//             configNode.setIdentity(freshIdentity);
-
-//             retries = 5;
-//             setTimeout(refresh, delay);
-//         } catch (error) {
-//             if (retries === 0) {
-//                 console.error(
-//                     'Could not refresh identity for external task worker processes. Stopping all external task workers.',
-//                     { error }
-//                 );
-//                 return;
-//             }
-//             console.error('Could not refresh identity for external task worker processes.', {
-//                 error,
-//                 retryCount: retries,
-//             });
-//             retries--;
-
-//             const delay = 2 * 1000;
-//             setTimeout(refresh, delay);
-//         }
-//     };
-
-//     await refresh();
-// }
