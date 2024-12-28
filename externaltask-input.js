f0787cca5ef2f6e5ebe758129dd73815fb32a594
Martin Möllenbeck <martin.moellenbeck@5minds.de>
2024-12-28 10:57:02 +0100

    Remove msg and format from etw result

--- externaltask-input.js
+++ externaltask-input.js
@@ -53,6 +53,10 @@ module.exports = function (RED) {
                     const handleFinishTask = (msg) => {
                         let result = RED.util.encodeObject(msg.payload);
 
+                        // remote msg and format from result
+                        delete result.format;
+                        delete result.msg;
+
                         node.log(
                             `handle event for *external task flowNodeInstanceId* '${externalTask.flowNodeInstanceId}' and *processInstanceId* ${externalTask.processInstanceId} with result ${JSON.stringify(result)} on msg._msgid ${msg._msgid}.`
                         );
