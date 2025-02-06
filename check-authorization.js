581893eb3d227bbd1e9347b05aace1278578e751
luisthieme <luis.thieme@5minds.de>
2025-02-06 15:24:27 +0100

    check for claim name

--- check-authorization.js
+++ check-authorization.js
@@ -1,10 +1,25 @@
 module.exports = function (RED) {
     function CheckAuthorization(config) {
         RED.nodes.createNode(this, config);
-        var node = this;
+        const node = this;
+
+        const claimsToCheck = (config.options || []).map((option) => option.claim);
 
         node.on('input', function (msg) {
-            node.send(msg)
+            if (!msg._client || !msg._client.user || typeof msg._client.user.claims !== "object") {
+                node.error("Invalid client claims in the input message", msg);
+                return;
+            }
+
+            const userClaims = msg._client.user.claims;
+
+            const isAuthorized = claimsToCheck.every((claim) => claim in userClaims);
+
+            if (isAuthorized) {
+                node.send([msg, undefined]);
+            } else {
+                node.send([undefined, msg]);
+            }
         });
     }
 
