da1eecfbebc9d78c91ebe4b6eef2aaa3b89bf67c
luisthieme <luis.thieme@5minds.de>
2025-02-06 10:25:07 +0100

    add basic node

---
+++ check-authorization.js
@@ -0,0 +1,12 @@
+module.exports = function (RED) {
+    function CheckAuthorization(config) {
+        RED.nodes.createNode(this, config);
+        var node = this;
+
+        node.on('input', function (msg) {
+            node.send(msg)
+        });
+    }
+
+    RED.nodes.registerType('check-authorization', CheckAuthorization);
+};
