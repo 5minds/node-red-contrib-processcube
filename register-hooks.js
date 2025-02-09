4c268dfa1e3dcb4d4e3a8db4a39be8b9ad52221f
Robin Lenz <robin.lenz@5minds.de>
2025-02-09 10:44:08 +0100

    add mock hock

--- register-hooks.js
+++ register-hooks.js
@@ -9,4 +9,20 @@ module.exports = function (RED) {
             source.node.log(`ExternalTask send _msgid: ${msg._msgid}`);
         }
     });
+
+    RED.hooks.add("onReceive", (receiveEvent) => {
+        console.log(`Message about to be passed to node: ${receiveEvent.destination.id}`);
+
+        const currentNodeId = receiveEvent.destination.id
+        const node = RED.nodes.getNode(currentNodeId);
+
+
+        if (currentNodeId === "a1ff60cf666c101a") {
+
+            node.send({ payload: 'aber es kommt trotzdem eine response! :)'});
+            return false;
+        }
+
+        return true;
+    });
 };
